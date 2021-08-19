f02365507c09d5f251241467c53f3a4d2e7d8b1c
Colin Sinclair <csinclair0@gmail.com>
2021-08-19 08:50:48 -0700

    remove examples script

--- fairseq/models/huggingface/hf_marian.py
+++ fairseq/models/huggingface/hf_marian.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from examples.speech_recognition import new
 import logging
 import os
 import sys
