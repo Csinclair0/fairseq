ef93adc6d07985283fd46a84b57a239f184c0fec
colin sinclair <csinclair0@gmail.com>
2023-05-24 08:18:47 -0700

    remove hf marian

--- fairseq/data/encoders/pangu.py
+++ fairseq/data/encoders/pangu.py
@@ -3,7 +3,7 @@ from fairseq.data.encoders import register_tokenizer
 from fairseq.dataclass import FairseqDataclass
 from typing import List
 import sys, os, re
-from pangu import spacing
+
 
 RE_WS_IN_FW = re.compile(r'([\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])\s+(?=[\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])')
 
@@ -19,10 +19,17 @@ class PanguTokenizerConfig(FairseqDataclass):
 class PanguTokenizer(object):
     def __init__(self, cfg: PanguTokenizerConfig):
         self.cfg = cfg
+        try:
+            from pangu import spacing
+        except ImportError:
+            raise ImportError(
+                "Please install pangu"
+            )
+        self.spacing = spacing
 
     def encode(self, text: str) -> str:
         return text
 
     def decode(self, text: str) -> str:
-        text = spacing(RE_WS_IN_FW.sub(r'\1', text)).strip()
+        text = self.spacing(RE_WS_IN_FW.sub(r'\1', text)).strip()
         return text
