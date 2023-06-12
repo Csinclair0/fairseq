69986196d99c7b4ecb027ad9d0a7fa8955dd227b
colin sinclair <csinclair0@gmail.com>
2023-06-12 14:43:28 -0700

    ignore is a directory error

--- fairseq/checkpoint_utils.py
+++ fairseq/checkpoint_utils.py
@@ -163,15 +163,10 @@ def save_checkpoint(
                 ), f"Failed to copy {src} to {dest}"
 
         for cp in checkpoints[1:]:
-            copy_or_symlink(src=checkpoints[0], dest=cp)
-        #    if (trainer.is_moe or trainer.is_base_moe) and (
-        #        trainer.is_data_parallel_master
-        #        or (trainer.is_fsdp and trainer.use_sharded_state)
-        #    ):
-        #        copy_or_symlink(
-        #            src=re.sub("rank-[0-9]+", "shared", checkpoints[0]),
-        #            dest=re.sub("rank-[0-9]+", "shared", cp),
-        #        )
+            try:
+                copy_or_symlink(src=checkpoints[0], dest=cp)
+            except IsADirectoryError:
+                pass
 
         write_timer.stop()
         logger.info(
